6e4cf79a6a8a7c6dedaf1d7ff55fd985adda3dfb
ピーポッポ <157344590+nightingalemusk@users.noreply.github.com>
2024-02-16 20:14:51 +0900

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TextConverter from './components/TextConverter';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TextConverter />
+    </div>
+  );
+}
+
+export default App;
